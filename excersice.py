003e85c67b65cafff5a1bbf3c75b9904db2e0827
Ryan <rzhou058@gmail.com>
2024-05-04 01:51:22 +0800

    '"changes"

---
+++ excersice.py
@@ -0,0 +1,79 @@
+import mediapipe as mp
+import cv2
+
+class pose():
+    def __init__(self):
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_pose = mp.solutions.pose
+        self.pose = self.mp_pose.Pose(static_image_mode=False, 
+                    model_complexity=1, 
+                    enable_segmentation=False, 
+                    min_detection_confidence=0.5)
+        
+    def detect_pose(self, img, result):
+    # 检查是否检测到姿态
+        if result.pose_landmarks:
+            # 绘制关键点和骨架
+            self.mp_drawing.draw_landmarks(img, result.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
+
+        return img
+
+    def find_pose(self, img, result):
+        lmlist = list()
+        if result.pose_landmarks:   
+            for id, lm in enumerate(result.pose_landmarks.landmark):
+                # print("1")
+                h, w, c = img.shape 
+                cx, cy = int(w * lm.x), int(h * lm.y)
+                lmlist.append([cx, cy])
+
+        return lmlist
+
+    def Squat(self, img, lmlist, point, flag):
+        pos1 = lmlist[11][1]
+        pos2 = lmlist[25][1]
+
+        if abs(pos1 - pos2) < 200:
+            if(flag == False):
+                point += 1
+                flag = True
+        else:
+            flag = False
+        
+        cv2.putText(img, "point = " + str(point), (30,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        return img, point, flag
+
+    def LiftFeet(self, img, lmlist, point, flag):
+        leftHip = lmlist[23][1]
+        leftKnee = lmlist[25][1]
+        rightHip = lmlist[24][1]
+        rightKnee = lmlist[26][1]
+
+        # if 
+        if rightHip - rightKnee > 10 or leftHip - leftKnee > 10:
+            if(flag == False):
+                point += 1
+                flag = True
+        else:
+            flag = False
+        
+        cv2.putText(img, "point = " + str(point), (30,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        return img, point, flag
+
+    def jumpingJacks(self, img, lmlist, point, flag):
+        leftHand = lmlist[19][0]
+        rightHand = lmlist[20][0]
+        leftelbow = lmlist[13][1]
+        rightelbow = lmlist[14][1]
+        leftshoulder = lmlist[11][1]
+        rightshoulder = lmlist[12][1]
+
+        if leftelbow < leftshoulder and rightelbow < rightshoulder and leftHand - rightHand < 15:
+            if(flag == False):
+                point += 1
+                flag = True
+        else:
+            flag = False
+
+        cv2.putText(img, "point = " + str(point), (30,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        return img, point, flag
--- final.py
+++ final.py
@@ -1,49 +1,40 @@
 import cv2
-import mediapipe as mp
 import time
-
-# 初始化 MediaPipe 姿态检测
-mp_pose = mp.solutions.pose
-ptime = 0
-pose = mp_pose.Pose(static_image_mode=False, 
-                    model_complexity=1, 
-                    enable_segmentation=False, 
-                    min_detection_confidence=0.5)
-
-# 初始化 MediaPipe 绘图工具
-mp_drawing = mp.solutions.drawing_utils
-
-def detect_pose(img):
-    # 将 BGR 帧转换为 RGB
-    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # 处理图像并获取姿态结果
-    result = pose.process(rgb_img)
-    # 检查是否检测到姿态
-    if result.pose_landmarks:
-        # 绘制关键点和骨架
-        mp_drawing.draw_landmarks(img, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-    return img
+import excersice as ex
 
 # 使用 OpenCV 从摄像头捕捉视频
 cap = cv2.VideoCapture(0)
 
-while cap.isOpened():
+ptime = 0
+point = 0
+flag = True
+excer = ex.pose()
+
+while True:
     ret, img = cap.read()
     if not ret:
-        continue
-    
+        print("failed")
+    img = cv2.flip(img,1)
+
+    # 将 BGR 帧转换为 RGB
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # 处理图像并获取姿态结果
+    result = excer.pose.process(rgb_img)
+
     # 检测姿态并绘制结果
-    annotated_frame = detect_pose(img)
-    
+    img = excer.detect_pose(img, result)
+    lmlist = excer.find_pose(img, result)
+
+    if(len(lmlist) != 0):   
+        # img, point, flag = excer.jumpingJacks(img, lmlist, point, flag)
+        # img, point, flag = excer.LiftFeet(img, lmlist, point, flag)
+        img, point, flag = excer.Squat(img, lmlist, point, flag)
+
     ctime = time.time()
     fps = 1/(ctime - ptime)
     ptime = ctime
 
-    cv2.putText(img, "fps: " + str(int(fps)), (30, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, )
+    cv2.putText(img, "fps: " + str(int(fps)), (30, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
     # 显示处理后的视频帧
     cv2.imshow("Image", img)
     cv2.waitKey(2)
-
-# 释放资源并关闭窗口
-cap.release()
-cv2.destroyAllWindows()
