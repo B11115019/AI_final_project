b947c05816d967e01da574a2204a06c186953681
jeang-bo-yuan <jeangbyj@gmail.com>
2024-05-07 23:20:41 +0800

    重構選擇動作

--- final.py
+++ final.py
@@ -18,82 +18,120 @@ for i in range(len(mylist)):
     mylist[i] = cv2.resize(mylist[i], (128, 720)) # width = 128, height = 720
 
 
+def capture_and_detect_pose():
+    """
+    從相機讀入一幀，並做姿態檢測 + 繪製骨架
+    ## Return:
+    - img - 圖片
+    - lmlist - 檢測到的骨架的節點
+    """
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+    assert(img.shape == (720, 1280, 3))
+
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # 处理图像并获取姿态结果
+    result = excer.pose.process(rgb_img)
+
+    # 检测姿态并绘制结果
+    img = excer.detect_pose(img, result)
+    lmlist = excer.find_pose(img, result)
+
+    return img, lmlist
+
+
 def select_exercise_with_cam() -> ExerciseChoise:
     """
     使用相機來選擇動作
     """
     idx = 0
-    start_time = 0
-    break_time = 0
+    start_time = time.time()
 
     while True:
+        # 目前的時間
         ctime = time.time()
+        # 經過的時間
+        pass_time = int(ctime - start_time)
 
-        ret, img = cap.read()
-        img = cv2.flip(img, 1)
-        assert(img.shape == (720, 1280, 3))
+        img, lmlist = capture_and_detect_pose()
+
+        # 顯示倒數秒數
+        if idx != GUIIndex.DEFAULT:
+            cv2.putText(img, str(2 - pass_time), (300, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+
         # set up GUI
         img[0:720, 0:128] = mylist[idx]
-        
-        rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        # 处理图像并获取姿态结果
-        result = excer.pose.process(rgb_img)
-
-        # 检测姿态并绘制结果
-        img = excer.detect_pose(img, result)
-        lmlist = excer.find_pose(img, result)
-
-        if len(lmlist) != 0:
-            right_index_x = lmlist[20][0]
-            right_index_y = lmlist[20][1]
-
-            # 判斷是否確定要選擇這個運動
-            if idx != GUIIndex.DEFAULT and int(ctime - start_time) == 2:
-                if break_time == 0:
-                    break_time = time.time()
-
-                if idx == GUIIndex.SQUAT_HOVER:
-                    idx = GUIIndex.SQUAT
-                elif idx == GUIIndex.SIT_UP_HOVER:            
-                    idx = GUIIndex.SIT_UP
-                elif idx == GUIIndex.JUMP_HOVER:
-                    idx = GUIIndex.JUMP
-                elif idx == GUIIndex.LIFT_FEET_HOVER:
-                    idx = GUIIndex.LIFT_FEET
-            
-            if idx == GUIIndex.SQUAT or idx == GUIIndex.SIT_UP or idx == GUIIndex.JUMP or idx == GUIIndex.LIFT_FEET:
-                cv2.putText(img, str(2 - int(ctime - break_time)), (300, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-                if int(ctime - break_time) == 2:
-                    if idx == GUIIndex.SQUAT:
-                        return ExerciseChoise.SQUAT
-                    elif idx == GUIIndex.SIT_UP:
-                        return ExerciseChoise.SIT_UP
-                    elif idx == GUIIndex.JUMP:
-                        return ExerciseChoise.JUMP
-                    else:
-                        return ExerciseChoise.LIFT_FEET
-                
-            # 移動選擇運動類型
-            if right_index_x < 128:
-                if 2 - int(ctime - start_time) > 0:
-                    cv2.putText(img, str(2 - int(ctime - start_time)), (300, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-                
-                if break_time == 0:
-                    if right_index_y < 120:
-                        idx = GUIIndex.SQUAT_HOVER
-                    elif right_index_y < 320:
-                        idx = GUIIndex.SIT_UP_HOVER
-                    elif right_index_y < 465:
-                        idx = GUIIndex.JUMP_HOVER
-                    elif right_index_y < 595:
-                        idx = GUIIndex.LIFT_FEET_HOVER
-            else:
-                start_time = time.time()
-                idx = 0
-                break_time = 0
 
         cv2.imshow("Image", img)
         cv2.waitKey(2)
+
+        if len(lmlist) == 0 :
+            continue
+
+        # state transtition #########################
+        # Steps
+        # 1. 先檢查有沒有選中不同的動作
+        # 2. 檢查有沒有過兩秒
+        right_index_x = lmlist[20][0]
+        right_index_y = lmlist[20][1]
+
+        # 移動選擇運動類型
+        if right_index_x < 128:
+            if right_index_y < 120:
+                # 從其他運動改選 SQUAT
+                if idx != GUIIndex.SQUAT_HOVER and idx != GUIIndex.SQUAT:
+                    start_time = ctime
+                    idx = GUIIndex.SQUAT_HOVER
+                    continue
+            elif right_index_y < 320:
+                # 從其他動作改選 SIT_UP
+                if idx != GUIIndex.SIT_UP_HOVER and idx != GUIIndex.SIT_UP:
+                    start_time = ctime
+                    idx = GUIIndex.SIT_UP_HOVER
+                    continue
+            elif right_index_y < 465:
+                # 從其他動作改選 JUMP
+                if idx != GUIIndex.JUMP_HOVER and idx != GUIIndex.JUMP:
+                    start_time = ctime
+                    idx = GUIIndex.JUMP_HOVER
+                    continue
+            elif right_index_y < 595:
+                # 從其他動作改選 LIFT_FEET
+                if idx != GUIIndex.LIFT_FEET_HOVER and idx != GUIIndex.LIFT_FEET:
+                    start_time = ctime
+                    idx = GUIIndex.LIFT_FEET_HOVER
+                    continue
+        else:
+            # 沒有選中任何動作
+            start_time = ctime
+            idx = GUIIndex.DEFAULT
+            continue
+
+        # 檢查有沒有過2秒
+        if pass_time >= 2:
+            if idx == GUIIndex.SQUAT_HOVER:
+                start_time = ctime
+                idx = GUIIndex.SQUAT
+            elif idx == GUIIndex.SQUAT:
+                return ExerciseChoise.SQUAT
+
+            elif idx == GUIIndex.SIT_UP_HOVER:
+                start_time = ctime
+                idx = GUIIndex.SIT_UP
+            elif idx == GUIIndex.SIT_UP:
+                return ExerciseChoise.SIT_UP
+
+            elif idx == GUIIndex.JUMP_HOVER:
+                start_time = ctime
+                idx = GUIIndex.JUMP
+            elif idx == GUIIndex.JUMP:
+                return ExerciseChoise.JUMP
+            
+            elif idx == GUIIndex.LIFT_FEET_HOVER:
+                start_time = ctime
+                idx = GUIIndex.LIFT_FEET
+            elif idx == GUIIndex.LIFT_FEET:
+                return ExerciseChoise.LIFT_FEET
 pass # select_exercise_with_cam
 
 
